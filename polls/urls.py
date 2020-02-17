59a637ac2c345828334ca8bc306bdea86beda675
mayronh <mayron.hen@gmail.com>
2020-02-17 16:54:15 -0200

    criando o formulário para votação e mudando views para genéricas

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView, name='index'),
+    path('<int:pk>/', views.DetailView, name='detail'),
+    path('<int:pk>/results/', views.ResultsView, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 # from django.template import loader
-from .models import Question
+from .models import Question, Choice
 # Create your views here.
 
 
@@ -23,9 +25,9 @@ from .models import Question
 #     return render(request, 'polls/index.html', context)
 
 #Código mais limpo
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'lastest_question_list': lastest_question_list})
+# def index(request):
+#     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     return render(request, 'polls/index.html', {'lastest_question_list': lastest_question_list})
 
 #Mostrando um objeto ou mostrando uma página 404 caso não encontrado
 # def detail(request,question_id):
@@ -36,14 +38,49 @@ def index(request):
 #     return render(request, 'polls/detail.html', {'question':question})
 
 #Mesmo código mas utilizando um atalho
-def detail(request,question_id):
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question':question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    # Try catch verifica se a opção escolhida existe
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Se não existir retorna para a votação
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "Você não escolheu uma opção.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #Para previnir que usuário envie o mesmo formulário clicando no botão de voltar a página
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))  
 
-def results(request, question_id):
-    return HttpResponse("Resultados da questão %s." % question_id)
+#Como index, detail e result têm o mesmo código, melhor utilizar views genéricas:
 
-def vote(request,question_id):
-    return HttpResponse("Votação: %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 
